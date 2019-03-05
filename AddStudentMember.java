6da0c58cf765d0137bf442d6d3a3d1ba1c4b54ec
fazlerabbi2248 <42741131+fazlerabbi2248@users.noreply.github.com>
2019-03-06 00:07:13 +0600

    Update

--- AddStudentMember.java
+++ AddStudentMember.java
@@ -8,6 +8,49 @@ import java.awt.event.ActionListener;
 public class AddStudentMember extends AddMember{
     
     AddStudentMember(){
+        
+        add.addActionListener(new ActionListener(){
+        
+  
+        @Override
+        public void actionPerformed(ActionEvent e){
+            String sname =name.getText();
+            String sfather =Father.getText();
+            String  smother = mother.getText();
+            String  shometown = hometown.getText();
+            String  semail = email.getText();
+            String scontact = contact.getText();
+            String sblood = blood.getText();
+            
+            if(e.getSource()==add)
+            {
+                filewriter(sname,sfather,smother,shometown,semail,scontact,sblood);
+            }
+  
+        }
+
+            private void filewriter(String sname, String sfather, String smother, String shometown, String semail, String scontact, String sblood) {
+                
+                try{
+                    FileWriter wr = new FileWriter("student.txt",true);
+                    wr.write(sname+" "+" ");
+                    wr.write(sfather+" "+" ");
+                    wr.write(smother+" "+" ");
+                    wr.write(shometown+" "+" ");
+                    wr.write(semail+" "+" ");
+                    wr.write(scontact+" "+" ");
+                    wr.write(sblood+" "+" ");
+                    
+                    wr.write(System.getProperty("line.separator"));
+                    wr.close();
+                }
+                catch(Exception ae)
+                {
+                    System.out.println(ae);
+                }
+            }
+        
+        });
       
         home.addActionListener(new ActionListener(){
         
